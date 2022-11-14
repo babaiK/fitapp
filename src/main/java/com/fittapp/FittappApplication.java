169d9195d638404b6f8a305da39235560fa7f562
babaiK <64838333+babaiK@users.noreply.github.com>
2022-11-14 18:13:54 +0100

    init

---
+++ src/main/java/com/fittapp/FittappApplication.java
@@ -0,0 +1,13 @@
+package com.fittapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FittappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FittappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fittapp/FittappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fittapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FittappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
