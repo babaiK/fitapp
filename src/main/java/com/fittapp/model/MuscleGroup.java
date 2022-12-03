7d58a0cfc1d9ecd8c08c171d25683a7be126d17a
babaiK <64838333+babaiK@users.noreply.github.com>
2022-12-03 18:23:35 +0100

    add spaces to pojo

--- src/main/java/com/fittapp/model/MuscleGroup.java
+++ src/main/java/com/fittapp/model/MuscleGroup.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import javax.persistence.criteria.CriteriaBuilder;
 
 @Entity
 @Table(name="muscle_group")
@@ -15,12 +14,16 @@ public class MuscleGroup {
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     @Column
     private String name;
+
     @Column
     private Integer ppl; //1: push, 2: pull, 3: leg
+
     @Column
     private Integer lowerBody; //1: lowerbody, 0: upperbody
+
     @Column
     private Integer prio;
 }
